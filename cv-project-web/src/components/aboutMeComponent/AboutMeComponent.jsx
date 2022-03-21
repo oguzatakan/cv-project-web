9fd09e6c44d6f77e9f82904c4a838cb7f8eadb03
atakanoguz <atakan17171717@hotmail.com>
2022-03-22 00:51:47 +0300

    updates

--- cv-project-web/src/components/aboutMeComponent/AboutMeComponent.jsx
+++ cv-project-web/src/components/aboutMeComponent/AboutMeComponent.jsx
@@ -2,14 +2,38 @@ import "./AboutMeComponent.css";
 const AboutMeComponent=()=>{
     return(
     <div class = "hakkımdaMe">
-        <h1>Öz Geçmiş</h1>
+        <h2>Öz Geçmiş</h2>
         <p>01.01.2000 tarihinde Çanakkale'de doğdum. İlkokul, ortaokul, lise eğitimlerini Çanakkale'de tamamladım.
             Trakya Üniversitesi Bilgisayar Mühendisliği 3.Sınıf öğrencisiyim. 2009-2016 yılları arasında 7 sene profesyonel
             olarak Türk Telekom Basketbol takımında Gençler Liginde forma giydim. Lise yıllarımda 3 sene yarı profesyonel 
             olarak e-spor alanında turnuvalara katıldım.
         </p>
         <div>
-            <h1>İlgi Alanları</h1>
+            <h2>Yazılım Alanları</h2>
+    <ul class="list-group">
+<li class="list-group-item">Java</li>
+<li class="list-group-item">Spring Mvc</li>
+<li class="list-group-item">C#</li>
+<li class="list-group-item">React-JavaScript-Html-Css</li>
+</ul>
+    </div>
+    <div>
+            <h2>Dil Bilgisi</h2>
+    <ul class="list-group">
+<li class="list-group-item">Türkçe - Ana Dil</li>
+<li class="list-group-item">İngilizce - Orta Seviye</li>
+<li class="list-group-item">Almanca - Başlangıç</li>
+</ul>
+    </div>
+    <div>
+            <h2>Sertifikalar</h2>
+    <ul class="list-group">
+<li class="list-group-item">American Life C1 Certificate - American Life</li>
+<li class="list-group-item">Getek'20 - IEEE İstanbul Üniversitesi</li>
+</ul>
+    </div>
+        <div>
+            <h2>İlgi Alanları</h2>
             <p>2 yılı aşkın süredir Java SpringBoot üzerine backend alanında çalışmalar yapıyorum. Yaptığım çalışmalarımı
                 Github üzerinden public biçimde paylaşmaktayım. E-commerce, Spring Security vb. projelerim bulunmaktadır.
                 Frontend alanında son 2 aydır öğrenme aşamasındayım React alanında ilerleme hedeflerim bulunmaktadır.
@@ -17,7 +41,7 @@ const AboutMeComponent=()=>{
             </p>
         </div>
         <div>
-            <h1>Hobi</h1>
+            <h2>Hobi</h2>
             <p>Profesyonel olarak basketbol oynamaya devam edemesemde düzenli olarak devam etmekteyim. Bilgisayar 
                 oyunları hobilerim arasında bulunmaktadır. Yeni yerler keşfetmek, gezip yeni yerler hakkında bilgi
                 edinmek hobilerim arasında yer almaktadır.
