6df96d36fb818718d68575e9f9c45c19df4bc9cd
atakanoguz <atakan17171717@hotmail.com>
2022-03-22 04:29:41 +0300

    updates

--- cv-project-web/src/components/homePageComponent/HomePageComponent.jsx
+++ cv-project-web/src/components/homePageComponent/HomePageComponent.jsx
@@ -8,8 +8,9 @@ const HomePageComponent = () => {
           <img  src="https://media-exp1.licdn.com/dms/image/C4D03AQEzJh09oYps5Q/profile-displayphoto-shrink_200_200/0/1633082026317?e=1653523200&v=beta&t=Xs8Sb7iXzR7omQrVzGL81rV70FQlu7Qr0sbTX7Vj6Vg"
           height='300' width='300'></img>
         </div>
-        <div class="col aboutMe1">Merhaba, ben Atakan.Trakya Üniversitesi Bilgisayar Mühendisliği bölümü 3.Sınıf öğrencisiyim. Yazılım alanında Java SpringBoot ve ReactJs üzerine
-        çalışmalar gerçekleştiriyorum. Çeşitli projeler geliştirip, çeşitli projelerde yer alıyorum. </div>
+        <div class="col aboutMe1"><b>Merhaba, ben Atakan Oğuz.Trakya Üniversitesi Bilgisayar Mühendisliği bölümü 3.Sınıf öğrencisiyim. Yazılım alanında Java SpringBoot ve ReactJs üzerine
+        çalışmalar gerçekleştiriyorum. Çeşitli projeler geliştirip, çeşitli projelerde yer alıyorum.</b>
+        </div>
       </div>
     </div>
   );
--- cv-project-web/src/components/jobHistory/JobHistoryComponent.jsx
+++ cv-project-web/src/components/jobHistory/JobHistoryComponent.jsx
@@ -2,10 +2,18 @@ import "./JobHistoryComponent.css";
 const JobHistoryComponent=() =>{
     return(
     <div class = "jobMe">
+        
         <h1>İş Tecrübesi</h1>
-        <h3>Samsung Electronics</h3>
+
+        <div class ="row settıngs">
+            <img width={"60"} height={"30"} src = "https://3dwarehouse.sketchup.com/warehouse/v1.0/publiccontent/37941423-e2c5-4002-9643-e09861ccc11f"></img>
+            <h3>Samsung Electronics</h3>
+        </div>
         <p>2018-2019 yılları arasında Samsung Electronıcs Çanakkale Bayisinde satış danışmanlığı görevinde bulundum.</p>
-        <h3>Türk Telekom</h3>
+        <div class = "row settıngs">
+            <img width={"60"} height={"30"} src = "https://memurlar.net/common/news/images/731114/headline.jpg"></img>
+            <h3>Türk Telekom</h3>
+        </div>
         <p>2021-2022 yılları arasında Türk Telekom Telekomünikasyon Şirketinde Backend alanında part-time çalışan olarak rol aldım.</p>
 
     </div>
